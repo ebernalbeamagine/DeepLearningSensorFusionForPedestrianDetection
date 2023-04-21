4beece12a910c6c4178ba5b5955400a2a2c3fd29
acpl00 <chavezalfredo@hotmail.com>
2023-04-21 08:24:47 +0200

    pc radar to gray

--- dl_real_system_simulation/dl_pointcloud2gray/dl_pcradar2gray/src/dl_pcradar2gray.cpp
+++ dl_real_system_simulation/dl_pointcloud2gray/dl_pcradar2gray/src/dl_pcradar2gray.cpp
@@ -33,64 +33,41 @@ void RadarCallback(const  sensor_msgs::PointCloud2ConstPtr& radar_msg_){
     
    r_posx_ = camerar_point(0)/camerar_point(2);
    r_posy_ = camerar_point(1)/camerar_point(2);
-    
-   
-   
-    
-     
+  
    dr = sqrt(pow(radar_point(0), 2.0) + pow(radar_point(1), 2.0) + pow(radar_point(2), 2.0))/1;
    if(dr>35000)
        continue;
-    
-   // std::cout << " dr " << dr<< "\n";
+
     
     dr_int = (uint16_t)dr;
-      //getchar();
-     
+
      
     if((r_posx_ > W) || (r_posx_< 0))
          continue;
      if((r_posy_ > H) || (r_posy_ < 0))
          continue;
-     
-   
+ 
    
     depthr_.at<uint16_t>(r_posy_,r_posx_) = dr_int;
-    
-    
-     ///////----feel the depth radar points with vertical lines----////////
-    
+
     
     for(int i = 0; i < H; i++){
          int j = r_posx_;
-        
-      
+  
             depthr_.at<uint16_t>(i,j)= dr_int;
-             // std::cout << " " << depthr_.at<uint16_t>(i,j);
-       
-      
+          
     } 
       
    }
    
-   ////////////////////////////////////////////////////////////////////////
+  ////////////////////////////////////////////////////////////////////////
  ///////---convert depth to a ROS image and send it for the SegNet---////
  //////////////////////////////////////////////////////////////////////// 
    depth_image          = cv::Mat::zeros(H,W,CV_16UC1);
    depth_image_double   = cv::Mat::zeros(H,W,CV_16UC1);
    depth_image_gray     = cv::Mat::zeros(H,W,CV_16UC1);
    depth_image_gray8    = cv::Mat::zeros(H,W,CV_8UC1);
- 
- 
- //  for(int i = 0; i < H; i++){
- //     for(int j = 0; j < W; j++){   
- //        if(depthr_.at<uint16_t>(i,j) != 0){
- //           depth_image.row(i).col(j) =  depthr_.at<uint16_t>(i,j);
- //           //std::cout << " " << depth_image.row(i).col(j);
- //        }    
- //      }
- //   }
-    
+
  
     for (int i = 0; i < H; i++) {
     uint16_t* depth_ptr = depthr_.ptr<uint16_t>(i);
@@ -103,22 +80,10 @@ void RadarCallback(const  sensor_msgs::PointCloud2ConstPtr& radar_msg_){
         depth_image_ptr++;
     }
 }
-    
-    
+ 
    //Initializing max element as INT_MIN
     uint16_t maxElement = 0;
  
-    // checking each element of matrix
-    // if it is greater than maxElement,
-    // update maxElement 
-    //for (int i = 0; i < H; i++) {
-    //    for (int j = 0; j < W; j++) {
-    //        if (depth_image.at<uint16_t>(i,j) > maxElement) {
-    //            maxElement = depth_image.at<uint16_t>(i,j);
-    //        }
-    //    }
-    //}
-    
     const uint16_t* ptr = depth_image.ptr<uint16_t>(0);
     const uint16_t* end = ptr + H * W;
 
@@ -127,21 +92,7 @@ void RadarCallback(const  sensor_msgs::PointCloud2ConstPtr& radar_msg_){
         maxElement = *ptr;
        }
      }
-    
-    
-    
-    
-   //for(int i = 0; i < H; i++){
-   //   for(int j = 0; j < W; j++){ 
-   //      if(depthr_.at<uint16_t>(i,j) != 0){
-   //         //depth_image_double.at<uint16_t>(i,j)= (depth_image_double.at<uint16_t>(i,j)/maxElement)*255;
-   //         //depth_image_double.at<uint16_t>(i,j)= (depth_image_double.at<uint16_t>(i,j)/maxElement)*255;      
-   //         depth_image_double.at<uint16_t>(i,j)= (depth_image.at<uint16_t>(i,j)/1)*1;  
-   //         // std::cout << " " << depth_image_double.at<uint16_t>(i,j);        
-   //      }    
-   //   }
-   //}
-   
+ 
    for(int i = 0; i < H; i++){
        uint16_t* depth_ptr = depthr_.ptr<uint16_t>(i);
        uint16_t* depth_image_ptr = depth_image_double.ptr<uint16_t>(i);
@@ -157,14 +108,7 @@ void RadarCallback(const  sensor_msgs::PointCloud2ConstPtr& radar_msg_){
    
     
    depth_image_double.convertTo(depth_image_double, CV_64F);
-   
-   //for(int i = 0; i < H; i++){
-   //    for(int j = 0; j < W; j++){ 
-   //    depth_image_gray.row(i).col(j)    =int16_t((depth_image_double.at<double>(i,j)/float(maxElement))*65536);      
-   //   // std::cout << " " <<      depth_image_gray.row(i).col(j);
-   //    }
-   // }
-    
+
     
      for (int i = 0; i < H; i++) {
         int16_t* depth_row = depth_image_gray.ptr<int16_t>(i);
@@ -173,33 +117,16 @@ void RadarCallback(const  sensor_msgs::PointCloud2ConstPtr& radar_msg_){
             depth_row[j] = int16_t((depth_double_row[j] / float(maxElement)) * 65536);
        }
    }
-    
-    
-    
+ 
    //Create image color
    cv::Mat image_rgb(H, W, 3); 
-   
-   
-   
- 
-   
-  
+
    // convert the gray image to rgb
    if (depth_image_gray.type()==CV_16UC1){ 
       cvtColor(depth_image_gray, image_rgb, CV_GRAY2BGR);
-      //std::cout<<  "depth_image_gray.type()" <<  depth_image_gray.type() <<" ----- " <<image_rgb.type()<<"\n";  
    }
    
-   
-   
-   
-   //printf("Matrix: %s %dx%d \n", ty.c_str(), image_rgb.cols, image_rgb.rows );
-      
-  //   cv::imwrite("/home/acp/catkin_ws/src/real_system_simulation/pointcloud2gray/Lidar/"  + std::to_string(radar_cnt*0.000001).substr(8-leading) + ".png", depth_image_gray);  
-      
-      
-  ///// cv::Mat depth16_interpolated = interpolate(depth_image_gray);     
-      
+ 
    cv_bridge::CvImage img_bridge;
    sensor_msgs::Image img_msg; // >> message to be sent
 
@@ -210,242 +137,13 @@ void RadarCallback(const  sensor_msgs::PointCloud2ConstPtr& radar_msg_){
    //img_bridge = cv_bridge::CvImage(header, sensor_msgs::image_encodings::MONO16, depth16_interpolated);
    img_bridge = cv_bridge::CvImage(header, sensor_msgs::image_encodings::MONO16, depth_image_gray);
    
-   ///////img_bridge = cv_bridge::CvImage(header, sensor_msgs::image_encodings::MONO16, depth_image_gray);
-   //img_bridge = cv_bridge::CvImage(header, sensor_msgs::image_encodings::BGR8, image_rgb);
    img_bridge.toImageMsg(img_msg); // from cv_bridge to sensor_msgs::Image
    
    
-   img_pub_.publish(img_msg); // ros::Publisher pub_img = node.advertise<sensor_msgs::Image>("topic", queuesize);   
+   img_pub_.publish(img_msg); 
     
    radar_cnt++;
-//pc_pub_.publish(pc_);
    
-         
-   
-}
-
-cv::Mat interpolate(cv::Mat depth_image_gray){
-
-  lim=20;
-    for(int i = 1; i < H; i++){
-        for(int j = 1; j < W; j++){ 
-           int16_t point= static_cast<int16_t>(depth_image_gray.at<uint16_t>(i,j));
-           //cv::Mat point = depth_image_gray.row(i).col(j);
-           //int x1 = static_cast<int>(detectionMat.at<float>(i, 3) * frameWidth);
-           //std::cout << "  " <<  point;
-           int x = i;
-           int y = j;
-              if (point == 0){
-                 
-                 //---calculate p1
-                 int ci=0;
-                 int cj=0;
-                 do{
-                 ci++;
-                 cj++;
-                 
-                 if(((i-ci) <= 1) || ((j+cj)>=W)){
-                     //std::cout << " cci  " <<  ci+i << "\n";
-                     break;
-                  }
-                  
-                  if(ci>lim)
-                    break;
-                 p1 = static_cast<int16_t>(depth_image_gray.at<uint16_t>(i-ci,j+cj));
-                 }while((p1==0));
-                 //std::cout << " p1  " <<  p1 << "\n";
-                 
-                 //---calculate p2
-                 ci=0;
-                 cj=0;
-                 do{
-                 ci++;
-                
-                 //std::cout << " cj  " <<  cj << "\n";
-                 //std::cout << " p2  " <<  p2 << "\n";
-                  //iic=i+ci;
-                  //std::cout << " cii  " <<  ci+i << "\n";
-                  
-                  if((i+ci) >= H){
-                     //std::cout << " ci  " <<  ci+i << "\n";
-                     break;
-                  }
-                  if(ci>lim)
-                    break;
-                  
-                  p2 = static_cast<int16_t>(depth_image_gray.at<uint16_t>(i+ci,j));
-                  
-                  
-                 }while((p2==0));
-                 //std::cout << " p2  " <<  p2 << "\n";
-                 
-                 //---calculate p3
-                 ci=0;
-                 cj=0;
-                 do{
-                 ci++;
-                 cj++;
-                 
-                 
-                 if(((ci+i) >= H) || ((j+cj)>=W)){
-                     //std::cout << " cci  " <<  ci+i << "\n";
-                     break;
-                  }
-                 if(ci>2)
-                    break;
-                 
-                 p3 = static_cast<int16_t>(depth_image_gray.at<uint16_t>(i+1,j+1));
-                 }while((p3==0));
-                 //std::cout << " p3  " <<  p3 << "\n";
-                 
-                 
-                 //---calculate p4
-                 ci=0;
-                 cj=0;
-                 do{
-                 ci++;
-                 cj++;
-                 
-                 
-                 if( (j+cj) >= W){
-                     //std::cout << " cci  " <<  ci+i << "\n";
-                     break;
-                  }
-                 if(ci>lim)
-                    break;
-                 
-                 p4 = static_cast<int16_t>(depth_image_gray.at<uint16_t>(i,j+cj));
-                 }while((p4==0));
-                 //std::cout << " p4  " <<  p4 << "\n";
-                 
-                 
-                 //---calculate p5
-                 ci=0;
-                 cj=0;
-                 do{
-                 ci++;
-                 cj++;
-                 
-                 
-                 if(((i-ci) <= 1) || ((j+cj)>=W)){
-                     //std::cout << " cci  " <<  ci+i << "\n";
-                     break;
-                  }
-                 if(ci>lim)
-                    break;
-                 
-                 p5 = static_cast<int16_t>(depth_image_gray.at<uint16_t>(i-ci,j+cj));
-                 }while((p5==0));
-                 //std::cout << " p5  " <<  p5 << "\n";
-                 
-                 //---calculate p6
-                 ci=0;
-                 cj=0;
-                 do{
-                 ci++;
-                 cj++;
-                 
-                 
-                 if((i-ci) <= 1){
-                     //std::cout << " cci  " <<  ci+i << "\n";
-                     break;
-                  }
-                 if(ci>lim)
-                    break;
-                 
-                 p6 = static_cast<int16_t>(depth_image_gray.at<uint16_t>(i-ci,j));
-                 }while((p6==0));
-                 //std::cout << " p6  " <<  p6 << "\n";
-                 
-                 
-                 
-                 //---calculate p7
-                 ci=0;
-                 cj=0;
-                 do{
-                 ci++;
-                 cj++;
-                 
-                 if(((ci-i) <= 1) || ((j-cj)<=1)){
-                     //std::cout << " cci  " <<  ci+i << "\n";
-                     break;
-                  }
-                 if(ci>lim)
-                    break;
-                 
-                 p7 = static_cast<int16_t>(depth_image_gray.at<uint16_t>(i-ci,j-cj));
-                 }while((p7==0));
-                 //std::cout << " p7 " <<  p7 << "\n";
-                 
-                 //---calculate p8
-                 ci=0;
-                 cj=0;
-                 do{
-                 ci++;
-                 cj++;
-                 
-                 
-                 if((j-cj) <= 1){
-                     //std::cout << " cci  " <<  ci+i << "\n";
-                     break;
-                  }
-                 if(ci>lim)
-                    break;
-                 
-                 p8 = static_cast<int16_t>(depth_image_gray.at<uint16_t>(i,j-cj));
-                 }while((p8==0));
-                 //std::cout << " p8 " <<  p8 << "\n";
-                 
-                 
-                 //get the four smallest values
-                 //vector<int>  v{p1,p2,p3,p4,p5,p6,p7,p8};
-                 //sort(v.begin(), v.end());  
-                 
-                 //cout << "Sorted \n";
-                 //for (auto x : v)
-                 //   cout << x << " ";
-                 
-                 //p1 = static_cast<int>(depth_image_gray.at<uint8_t>(i-1,j+1));
-                 //p2 = static_cast<int>(depth_image_gray.at<uint8_t>(i+1,j));
-                 //p3 = static_cast<int>(depth_image_gray.at<uint8_t>(i+1,j+1));
-                 //p4 = static_cast<int>(depth_image_gray.at<uint8_t>(i,j+1));
-                 //p5 = static_cast<int>(depth_image_gray.at<uint8_t>(i-1,j+1));
-                 //p6 = static_cast<int>(depth_image_gray.at<uint8_t>(i-1,j));
-                 //p7 = static_cast<int>(depth_image_gray.at<uint8_t>(i-1,j-1));
-                 //p8 = static_cast<int>(depth_image_gray.at<uint8_t>(i,j-1));
-                 int16_t pav = 0;
-                 
-                 //get the four smallest values
-                 //vector<int>  v{p1,p2,p3,p4,p5,p6,p7,p8};
-                 //sort(v.begin(), v.end());
-                 
-                 
-                 //for (i=0; i<4;i++)
-                  // pav = (pav+v[i])/4;
-                   
-                 //std::cout << "pav   " <<  pav;  
-                  //pav=(v[0]+v[1]+v[2]+v[3])/4;
-                  pav =(p1+p2+p3+p4+p5+p6+p7+p8)/8; 
-                  
-                 // pav = v[0]*i + v[1]*j + v[3]*i*j+v[4];
-                  
-                 depth_interpolate.row(i).col(j) = pav;
-                 
-               //std::cout << "   " <<  pav;
-                  
-               //std::cout << " " <<      depth_interpolate.row(i).col(j);
-               //std::cout << "HERE";
-                //getchar();
-                }
-       }
-    }
-  
-    
-    
-    
-   
-   return depth_interpolate;
-
 }
 
 
@@ -490,30 +188,14 @@ int main(int argc, char **argv){
    
    ros::Rate loop_rate(10);
    
-   
-  
-   //radar_ = nh_.subscribe("/radar_sync", 10, RadarCallback);
-   
    radar_ = nh_.subscribe("/radar_simulation", 10, RadarCallback);
    img_pub_   = nh_.advertise<sensor_msgs::Image>("/radar_interpolated16", 2);  
-   //img_pub_   = nh_.advertise<sensor_msgs::Image>("/radar_simulation", 2);
+  
   
    int count = 0;
 
-while (ros::ok())
-  {
-    /**
-     * This is a message object. You stuff it with data, and then publish it.
-     */
-    //std_msgs::String msg;
-
-    //std::stringstream ss;
-    //ss << "hello world " << count;
-    //msg.data = ss.str();
-
-    //ROS_INFO("%s", msg.data.c_str());
-
-  
+while (ros::ok()){
+ 
 
     ros::spinOnce();
     loop_rate.sleep();
