b2009b5c8a27f5ef5296bf9181c93092f83492b6
acpl00 <chavezalfredo@hotmail.com>
2023-04-24 07:40:20 +0200

    pointcloud to 2D gray

--- dl_real_system_simulation/dl_pointcloud2gray/dl_pcradar2gray/src/dl_pcradar2gray.cpp
+++ dl_real_system_simulation/dl_pointcloud2gray/dl_pcradar2gray/src/dl_pcradar2gray.cpp
@@ -1,4 +1,33 @@
-
+/******************************************************************************
+ * \file
+ *
+ * $Id:$
+ *
+ * Copyright (C)  
+ *
+ * This file is part of software developed by UPC-Cd6-Beamagin group.
+ *
+ * Author: Alfredo Chávez Plascencia  alfredo.chavez@upc.edu
+ * Author: Pablo García Gómez        pablo.garcia@beamagine.com
+ * Author: Eduardo Bernal Perez      eduardo.bernal@upc.edu
+ * 
+ * This file is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This file is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this file.  If not, see <http://www.gnu.org/licenses/>.
+ */
+ 
+//////////////////////////////////////////////////////////////// 
+//---This node converts the radar pointcloud to 2D image  .---//
+////////////////////////////////////////////////////////////////
 
 
 #include <dl_pcradar2gray.h>
